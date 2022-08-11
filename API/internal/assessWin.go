e62e77d01a2a7fed509ef19eb307aa7afe8f0bbe
eisandbar <eisandbar@gmail.com>
2022-08-11 18:46:19 -0400

    Added a win assessor
    Function AssessWin takes in a complete set of player, opponent and board cards and determines who the winner is, if any.

---
+++ API/internal/assessWin.go
@@ -0,0 +1,137 @@
+package internal
+
+import (
+	"eisandbar/poker/typing"
+	"eisandbar/poker/util"
+	"fmt"
+	"sort"
+)
+
+// Given player hands and board cards, find the strongest combination for each player and decide who wins
+// For each board, a player has 31 possible combinations
+
+const (
+	High int = iota
+	Pair
+	TwoPair
+	Set
+	Straight
+	Flush
+	FullHouse
+	Quads
+	StraightFlush
+)
+
+// Given player and opponent hands and all boards cards assess who wins
+func AssessWin(playerHand, opponentHand, boardCards []string) (int, error) {
+	if len(boardCards) != 5 || len(playerHand) != 2 || len(opponentHand) != 2 {
+		return 0, util.BadInput
+	}
+
+	player, opponent, board, err := convertInputs(playerHand, opponentHand, boardCards)
+	if err != nil {
+		return 0, fmt.Errorf("Error converting inputs, %w", err)
+	}
+
+	if checkDuplicates(append(append(player, opponent...), board...)) {
+		return 0, fmt.Errorf("Duplicate cards detected, %w", err)
+	}
+
+	playerCombo, playerPriority := bestHand(player, board)
+	opponentCombo, opponentPriority := bestHand(opponent, board)
+	return compareCombo(playerCombo, opponentCombo, playerPriority, opponentPriority)
+}
+
+func convertInputs(playerHand, opponentHand, boardCards []string) ([]int, []int, []int, error) {
+	player, err := typing.ConvertStrings(playerHand)
+	if err != nil {
+		return nil, nil, nil, util.BadInput
+	}
+	opponent, err := typing.ConvertStrings(opponentHand)
+	if err != nil {
+		return nil, nil, nil, util.BadInput
+	}
+	board, err := typing.ConvertStrings(boardCards)
+	if err != nil {
+		return nil, nil, nil, util.BadInput
+	}
+	return player, opponent, board, nil
+}
+
+func checkDuplicates(input []int) bool {
+	sort.Ints(input)
+	for i := 1; i < len(input); i++ {
+		if input[i] == input[i-1] {
+			return true
+		}
+	}
+	return false
+}
+
+func bestHand(hand, board []int) (int, []int) {
+
+	combo, priority := comboValue(board)
+	for _, card := range hand {
+		for i, _ := range board {
+			temp := make([]int, 5)
+			copy(temp, board)
+			temp[i] = card
+			combo, priority = updateCombo(combo, priority, temp)
+		}
+	}
+	for i := range board {
+		for j := i + 1; j < len(board); j++ {
+
+			temp := make([]int, 5)
+			copy(temp, board)
+			temp[i] = hand[0]
+			temp[j] = hand[1]
+			combo, priority = updateCombo(combo, priority, temp)
+		}
+	}
+	return combo, priority
+}
+
+func updateCombo(combo int, priority, cards []int) (int, []int) {
+	newCombo, newPriority := comboValue(cards)
+	better, err := compareCombo(newCombo, combo, newPriority, priority)
+	if err == nil && better == 1 {
+		return newCombo, newPriority
+	}
+	return combo, priority
+}
+
+// Given 5 cards calculates their combo value
+func comboValue(cards []int) (int, []int) {
+	sort.Slice(cards, func(i, j int) bool { return cards[i] > cards[j] })
+	for _, checker := range checkers {
+		if combo, priority, is := checker(cards); is {
+			return combo, append(priority, getValues(cards)...)
+		}
+	}
+
+	return 0, cards
+}
+
+// Given two combo values, calculate which one is stronger
+// 1 means comboA is stronger, -1 meas comboB is stronger and 0 means a draw
+func compareCombo(comboA, comboB int, priorityA, priorityB []int) (int, error) {
+	if comboA > comboB {
+		return 1, nil
+	}
+	if comboA < comboB {
+		return -1, nil
+	}
+	if len(priorityA) != len(priorityB) {
+		return 0, util.BadInput
+	}
+	for i, value := range priorityA {
+		if value > priorityB[i] {
+			return 1, nil
+		}
+		if value < priorityB[i] {
+			return -1, nil
+		}
+	}
+	return 0, nil
+}
---
+++ API/internal/assessWin_test.go
@@ -0,0 +1,87 @@
+package internal_test
+
+import (
+	"eisandbar/poker/internal"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAssessWin(t *testing.T) {
+	playerHand := []string{"AH", "KH"}
+	opponentHand := []string{"QH", "9D"}
+	testBoards := []struct {
+		name       string
+		boardCards []string
+		res        int
+	}{
+		{"Ace High", []string{"JC", "TC", "5C", "2S", "3D"}, 1},
+		{"Ace Pair", []string{"AC", "TC", "5C", "2S", "3D"}, 1},
+		{"Nine Set", []string{"9C", "9H", "AC", "KS", "3D"}, -1},
+		{"Queen Pair", []string{"QC", "TC", "5C", "2S", "3D"}, -1},
+		{"King Pair", []string{"QC", "KC", "5C", "2S", "3D"}, 1},
+		{"Two Pair", []string{"QC", "KC", "9C", "2S", "3D"}, -1},
+		{"Straight", []string{"JC", "TC", "AC", "8S", "3D"}, -1},
+		{"Flush", []string{"JC", "TC", "8H", "2H", "3H"}, 1},
+		{"Better Kicker", []string{"JC", "JS", "JD", "JH", "2H"}, 1},
+		{"Draw", []string{"JC", "JS", "JD", "JH", "AS"}, 0},
+	}
+
+	for _, tt := range testBoards {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := internal.AssessWin(playerHand, opponentHand, tt.boardCards)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.res, got)
+		})
+	}
+}
+
+func TestAssessWinComplex(t *testing.T) {
+	playerHand := []string{"TH", "KH"}
+	opponentHand := []string{"TS", "QH"}
+	testBoards := []struct {
+		name       string
+		boardCards []string
+		res        int
+	}{
+		{"Pair Better Kicker", []string{"JC", "JD", "5C", "2S", "3D"}, 1},
+		{"Two Pair Better Kicker", []string{"JC", "JD", "TC", "2S", "3D"}, 1},
+		{"Better Pairs", []string{"9C", "9H", "TC", "KS", "AD"}, 1},
+		{"Same FullHouse", []string{"JC", "JD", "JS", "TC", "3D"}, 0},
+		{"Same FullHouse", []string{"JC", "JD", "JS", "AC", "AD"}, 0},
+		{"Better FullHouse", []string{"QC", "QS", "KC", "KS", "3D"}, 1},
+		{"Better FullHouse Queens", []string{"QC", "QS", "TC", "TD", "3D"}, -1},
+	}
+
+	for _, tt := range testBoards {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got, err := internal.AssessWin(playerHand, opponentHand, tt.boardCards)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.res, got)
+		})
+	}
+}
+
+func TestAssessWinBadInput(t *testing.T) {
+	playerHand := []string{"TH", "KH"}
+	opponentHand := []string{"TS", "QH"}
+	testBoards := []struct {
+		name       string
+		boardCards []string
+	}{
+		{"Repeating Ten", []string{"TH", "JD", "5C", "2S", "3D"}},
+		{"Repeating Queen", []string{"JC", "QH", "TC", "2S", "3D"}},
+		{"Repeating Board", []string{"9C", "9C", "TC", "KS", "AD"}},
+		{"Bad Card", []string{"JC", "FD", "JS", "TC", "3D"}},
+	}
+
+	for _, tt := range testBoards {
+		t.Run(tt.name, func(t *testing.T) {
+
+			_, err := internal.AssessWin(playerHand, opponentHand, tt.boardCards)
+			assert.Error(t, err)
+		})
+	}
+}
